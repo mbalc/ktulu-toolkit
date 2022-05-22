7a5955c8c625f00a003eb68ba3438b5456b86332
mbalc <mbalc909@gmail.com>
2022-05-22 21:58:16 +0200

    Add hot reload for i18n

--- next.config.js
+++ next.config.js
@@ -1,9 +1,27 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+// const { nextI18NextRewrites } = require('next-i18next/rewrites');
+
 const { i18n } = require('./next-i18next.config');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   i18n,
+  // rewrites: async () => nextI18NextRewrites(localeSubpaths),
+  webpack(config, options) {
+    if (!options.isServer && config.mode === 'development') {
+      const { I18NextHMRPlugin } = require('i18next-hmr/plugin');
+      config.plugins.push(
+        new I18NextHMRPlugin({
+          localesDir: path.resolve(__dirname, 'public/locales'),
+        })
+      );
+    }
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '../styles/globals.css';
+import { appWithTranslation, i18n } from 'next-i18next';
 import type { AppProps } from 'next/app';
-import { appWithTranslation } from 'next-i18next';
+import '../styles/globals.css';
+
+if (process.env.NODE_ENV === 'development') {
+  if (typeof window === 'undefined') {
+    const { applyServerHMR } = require('i18next-hmr/server');
+    applyServerHMR(i18n);
+  } else {
+    const { applyClientHMR } = require('i18next-hmr/client');
+    applyClientHMR(i18n);
+  }
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
